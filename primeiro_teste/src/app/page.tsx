9e0a598c71f1ba7d2f9518a5e1a60a72b70086f7
Davi Soares Dilly <93988870+DaviSoaresDilly@users.noreply.github.com>
2024-04-15 21:21:59 -0300

    atualizacao
    avanço considerável na compreensão do nextJs
    * foi feito a estruturação de pasta corretamente
    * o cabeçalho foi semi-criado

---
+++ primeiro_teste/src/app/page.tsx
@@ -0,0 +1,12 @@
+import "./globals.css";
+
+import Header from "./components/core/header"
+
+export default function Home() {
+  return (
+    <div className="App">
+      <Header />
+
+    </div>
+  );
+}
